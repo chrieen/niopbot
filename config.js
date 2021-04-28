6317f071938bf3325e8c667e34001e46019eb9b7
Glitch (marsh-brave-bottle) <none>
2021-04-28 12:31:25 +0000

    💡🛥 Checkpoint ./dashboard/templates/blocks/header.ejs:31375293/61 ./dashboard/templates/index.ejs:31375293/157 ./dashboard/templates/commands.ejs:31375293/13 ./dashboard/templates/admin.ejs:31375293/19 ./dashboard/templates/blocks/footer.ejs:31375293/66 ./dashboard/templates/blocks/guild-card.ejs:31375293/32 ./ayarlar.json:31375293/38 ./config.js:31375293/294

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const config = {
   "sahip": "795601207002660894",//SAHİP İD
 
   "dashboard" : {
-    "oauthSecret": "RW0sk2wihvqMOqPnurAzfJlgs0FW65Jc",// BOTUNUZUN SECRET IDSI
+    "oauthSecret": "G9-eDucVRLrNANLW9Fkf-T2wyejkRSP1",// BOTUNUZUN SECRET IDSI
     "callbackURL": "https://marsh-brave-bottle.glitch.me/callback",//CALLBACK URL ADRESİNİZ
-    "sessionSecret": "RW0sk2wihvqMOqPnurAzfJlgs0FW65Jc",//KALSIN DOKUNMAYIN
-    "domain": "https://marsh-brave-bottle.glitch.me",//PROJE LİNKİNİZ
+    "sessionSecret": "G9-eDucVRLrNANLW9Fkf-T2wyejkRSP1",
+    "domain": "https://marsh-brave-bottle.glitch.me/",//PROJE LİNKİNİZ
     "port": 8000
   },
 };
