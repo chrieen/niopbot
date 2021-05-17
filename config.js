2283d7bac4bd30e9f974ca062bc57e4c47d3dc14
Glitch (marsh-brave-bottle) <none>
2021-05-17 19:32:32 +0000

    💷❣️ Checkpoint ./dashboard/templates/dashboard.ejs:18445355/135 ./config.js:18445355/102 ./dashboard/templates/admin.ejs:18445355/2248

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const config = {
 
   "dashboard" : {
     "oauthSecret": "FP4L79p6QgYlnPnQ4iwgM3Lywg7Xxn7L",// BOTUNUZUN SECRET IDSI
-    "callbackURL": "https://abalone-apple-fear.glitch.me/callback",//CALLBACK URL ADRESİNİZ
+    "callbackURL": "http://abalone-apple-fear.glitch.me/callback",//CALLBACK URL ADRESİNİZ
     "sessionSecret": "FP4L79p6QgYlnPnQ4iwgM3Lywg7Xxn7L",
-    "domain": "https://discord.com",//PROJE LİNKİNİZ
+    "domain": "https://abalone-apple-fear.glitch.me",//PROJE LİNKİNİZ
     "port": 8000
   },
 };
